b3ee6876f2156edb047105c8144a0c9f5089b89f
auoHan <710543268@qq.com>
2022-04-24 18:11:21 +0800

    修改Home页面样式

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -16,4 +16,26 @@ import TopNav from '@/components/TopNav/TopNav.vue'
 </script>
 
 <style lang="scss" scoped>
+.banner {
+  padding: 100px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background: lightgreen;
+
+  > .actions {
+    padding: 8px 0;
+
+    a {
+      margin: 0 8px;
+      background: #fff;
+      display: inline-block;
+      height: $home-h;
+      line-height: $home-h;
+      border-radius: calc($home-h / 2);
+      padding: 0 8px;
+    }
+  }
+}
 </style>
