5236b0c830ec1e172e11cc34bb6b5b3c06151559
auoHan <710543268@qq.com>
2022-04-26 00:01:12 +0800

    路由切换后关闭aside

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,16 @@
 </template>
 
 <script setup lang="ts">
+import router from '@/router'
+
 const width = document.documentElement.clientWidth
 const asideVisible = ref(width > 500)
 provide('asideVisible', asideVisible)
+router.afterEach(() => {
+  if (width < 500) {
+    asideVisible.value = false
+  }
+})
 </script>
 
 <style>
