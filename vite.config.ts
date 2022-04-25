a141d532bed74cc352500fe885a2d3932dccf6a0
auoHan <710543268@qq.com>
2022-04-25 17:41:59 +0800

    封装svg

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import * as path from 'path'
 import viteCompression from 'vite-plugin-compression'
 import AutoImport from 'unplugin-auto-import/vite'
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 
 export default defineConfig({
   plugins: [
@@ -19,6 +20,12 @@ export default defineConfig({
       imports: ['vue', 'vue-router'], // 自动导入vue和vue-router相关函数
       dts: 'src/auto-import.d.ts' // 生成 `auto-import.d.ts` 全局声明
     }),
+    createSvgIconsPlugin({
+      // 指定需要缓存的图标文件夹
+      iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
+      // 指定symbolId格式
+      symbolId: 'icon-[dir]-[name]'
+    })
   ],
   resolve: {
     alias: {
