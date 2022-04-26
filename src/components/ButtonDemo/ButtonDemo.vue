38be165bf1009ec42566194d5e9e4c48ad045c64
auoHan <710543268@qq.com>
2022-04-26 18:05:05 +0800

    支持size/level/disabled/loading

--- src/components/ButtonDemo/ButtonDemo.vue
+++ src/components/ButtonDemo/ButtonDemo.vue
@@ -1,18 +1,59 @@
 <template>
   <p>button示例</p>
   <h2>示例1</h2>
-  <Button>
-    你好
-  </Button>
-  <Button theme="button">
-    你好
-  </Button>
-  <Button theme="text">
-    你好
-  </Button>
-  <Button theme="link">
-    你好
-  </Button>
+  <div>
+    <Button>你好</Button>
+    <Button theme="button">你好</Button>
+    <Button theme="link">你好</Button>
+    <Button theme="text">你好</Button>
+  </div>
+  <h2>示例2</h2>
+  <div>
+    <div>
+      <Button size="big">大大大</Button>
+      <Button>普普通</Button>
+      <Button size="small">小小小</Button>
+    </div>
+    <div>
+      <Button size="big" theme="link">大大大</Button>
+      <Button theme="link">普普通通</Button>
+      <Button size="small" theme="link">小小小</Button>
+    </div>
+    <div>
+      <Button size="big" theme="text">大大大</Button>
+      <Button theme="text">普普通通</Button>
+      <Button size="small" theme="text">小小小</Button>
+    </div>
+  </div>
+  <h1>示例3</h1>
+  <div>
+    <div>
+      <Button level="main">主要按钮</Button>
+      <Button>普通按钮</Button>
+      <Button level="danger">危险按钮</Button>
+    </div>
+    <div>
+      <Button level="main" theme="link">主要链接按钮</Button>
+      <Button theme="link">普通链接按钮</Button>
+      <Button level="danger" theme="link">危险链接按钮</Button>
+    </div>
+    <div>
+      <Button level="main" theme="text">主要文字按钮</Button>
+      <Button theme="text">普通文字按钮</Button>
+      <Button level="danger" theme="text">危险文字按钮</Button>
+    </div>
+  </div>
+  <h2>示例4</h2>
+  <div>
+    <Button disabled>禁用按钮</Button>
+    <Button disabled theme="link">禁用链接按钮</Button>
+    <Button disabled theme="text">禁用按钮</Button>
+  </div>
+  <h2>示例5</h2>
+  <div>
+    <Button loading>加载中</Button>
+    <Button>加载完毕</Button>
+  </div>
 </template>
 
 <script lang='ts' setup>
--- src/lib/Button/Button.vue
+++ src/lib/Button/Button.vue
@@ -1,13 +1,30 @@
 <template>
-  <button :class="`peach-theme-${theme}`" class="peach-button">
+  <button :class="classes" :disabled="props.disabled" class="peach-button">
+    <span v-if="props.loading" class="peach-loadingIndicator"></span>
     <slot/>
   </button>
 </template>
 
 <script lang='ts' setup>
 
-const props = withDefaults(defineProps<{ theme?: string }>(), {
-  theme: 'button'
+interface Props {
+  theme?: string
+  size?: string
+  level?: string
+  disabled?: boolean
+  loading?: boolean
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  theme: 'button',
+  size: 'normal',
+  level: 'normal',
+  disabled: false,
+  loading: false
+})
+
+const classes = computed(() => {
+  return `peach-theme-${props.theme} peach-size-${props.size} peach-level-${props.level}`
 })
 </script>
 <style lang="scss">
@@ -15,10 +32,13 @@ $h: 32px;
 $border-color: #d9d9d9;
 $color: #333;
 $radius: 4px;
+$red: red;
+$grey: grey;
 .peach-button {
   box-sizing: border-box;
   height: $h;
   padding: 0 12px;
+  margin-bottom: 12px;
   cursor: pointer;
   display: inline-flex;
   justify-content: center;
@@ -29,6 +49,7 @@ $radius: 4px;
   border: 1px solid $border-color;
   border-radius: $radius;
   box-shadow: 0 1px 0 fade-out(black, 0.95);
+  transition: background .4s;
 
   & + & {
     margin-left: 8px;
@@ -53,7 +74,8 @@ $radius: 4px;
     box-shadow: none;
     color: $theme-color;
 
-    &:hover, &:focus {
+    &:hover,
+    &:focus {
       color: lighten($theme-color, 10%);
     }
   }
@@ -63,9 +85,118 @@ $radius: 4px;
     box-shadow: none;
     color: inherit;
 
-    &:hover, &:focus {
-      background: darken(white, 5%);;
+    &:hover,
+    &:focus {
+      background: darken(white, 5%);
     }
+  }
+
+  &.peach-size-big {
+    font-size: 24px;
+    height: 48px;
+    padding: 0 16px;
+  }
+
+  &.peach-size-small {
+    font-size: 12px;
+    height: 20px;
+    padding: 0 4px;
+  }
+
+  &.peach-theme-button {
+    &.peach-level-main {
+      background: $theme-color;
+      color: white;
+      border-color: $theme-color;
+
+      &:hover,
+      &:focus {
+        background: darken($theme-color, 10%);
+        border-color: darken($theme-color, 10%);
+      }
+    }
+
+    &.peach-level-danger {
+      background: $red;
+      border-color: $red;
+      color: white;
+
+      &:hover,
+      &:focus {
+        background: darken($red, 10%);
+        border-color: darken($red, 10%);
+      }
+    }
+  }
+
+  &.peach-theme-link {
+    &.peach-level-danger {
+      color: $red;
+
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+
+  &.peach-theme-text {
+    &.peach-level-main {
+      color: $theme-color;
+
+      &:hover,
+      &:focus {
+        color: darken($theme-color, 10%);
+      }
+    }
+
+    &.peach-level-danger {
+      color: $red;
+
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+
+  &.peach-theme-button {
+    &[disabled] {
+      cursor: not-allowed;
+      color: $grey;
+
+      &:hover {
+        border-color: $grey;
+      }
+    }
+  }
+
+  &.peach-theme-link, &.peach-theme-text {
+    &[disabled] {
+      cursor: not-allowed;
+      color: $grey;
+    }
+  }
+
+  > .peach-loadingIndicator {
+    width: 14px;
+    height: 14px;
+    display: inline-block;
+    margin-right: 4px;
+    border-radius: 8px;
+    border-color: $theme-color $theme-color $theme-color transparent;
+    border-style: solid;
+    border-width: 2px;
+    animation: peach-spin 1s infinite linear;
+  }
+}
+
+@keyframes peach-spin {
+  0% {
+    transform: rotate(0deg)
+  }
+  100% {
+    transform: rotate(360deg)
   }
 }
 </style>
