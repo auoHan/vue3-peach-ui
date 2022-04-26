56de6a729b08e76ad15bba8bb916a82491fcc59f
auoHan <710543268@qq.com>
2022-04-26 15:43:27 +0800

    添加theme属性以及基本样式

--- src/components/ButtonDemo/ButtonDemo.vue
+++ src/components/ButtonDemo/ButtonDemo.vue
@@ -4,6 +4,15 @@
   <Button>
     你好
   </Button>
+  <Button theme="button">
+    你好
+  </Button>
+  <Button theme="text">
+    你好
+  </Button>
+  <Button theme="link">
+    你好
+  </Button>
 </template>
 
 <script lang='ts' setup>
--- src/lib/Button/Button.vue
+++ src/lib/Button/Button.vue
@@ -1,12 +1,51 @@
 <template>
-  <button>
+  <button :class="`theme-${theme}`" class="peach-button">
     <slot/>
   </button>
 </template>
 
 <script lang='ts' setup>
 
+const props = withDefaults(defineProps<{ theme?: string }>(), {
+  theme: 'button'
+})
 </script>
 <style lang="scss" scoped>
+$h: 32px;
+$border-color: #d9d9d9;
+$color: #333;
+$radius: 4px;
+.peach-button {
+  box-sizing: border-box;
+  height: $h;
+  padding: 0 12px;
+  cursor: pointer;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  white-space: nowrap;
+  background: white;
+  color: $color;
+  border: 1px solid $border-color;
+  border-radius: $radius;
+  box-shadow: 0 1px 0 fade-out(black, 0.95);
 
+  & + & {
+    margin-left: 8px;
+  }
+
+  &:hover,
+  &:focus {
+    color: $theme-color;
+    border-color: $theme-color;
+  }
+
+  &:focus {
+    outline: none;
+  }
+
+  &::-moz-focus-inner {
+    border: 0;
+  }
+}
 </style>
