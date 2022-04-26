79c81e4c78526ba1d29f24cb30e579611a07c453
auoHan <710543268@qq.com>
2022-04-26 14:39:10 +0800

    使用插槽

--- src/components/ButtonDemo/ButtonDemo.vue
+++ src/components/ButtonDemo/ButtonDemo.vue
@@ -1,12 +1,14 @@
 <template>
-  <div>
-    ButtonDemo
-  </div>
+  <p>button示例</p>
+  <h2>示例1</h2>
+  <Button>
+    你好
+  </Button>
 </template>
 
 <script lang='ts' setup>
 
-</script>
+import Button from '@/lib/Button/Button.vue'</script>
 
 <style lang="scss" scoped>
 
---
+++ src/lib/Button/Button.vue
@@ -0,0 +1,13 @@
+<template>
+  <button>
+    <slot/>
+  </button>
+</template>
+
+<script lang='ts' setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
