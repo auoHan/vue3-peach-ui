5121d68ae8b28763f26ee3c35cbe55bdec19af89
auoHan <710543268@qq.com>
2022-04-27 17:04:02 +0800

    写入基本css

--- src/lib/Tabs/Tabs.vue
+++ src/lib/Tabs/Tabs.vue
@@ -1,6 +1,12 @@
 <template>
-  <div v-for="(t,index) in titles" :key="index">{{ t }}</div>
-  <component :is="v" v-for="(v,index) in defaults" :key="index"/>
+  <div class="peach-tabs">
+    <div class="peach-tabs-nav">
+      <div v-for="(t,index) in titles" :key="index" class="peach-tabs-nav-item">{{ t }}</div>
+    </div>
+    <div class="peach-tabs-content">
+      <component :is="v" v-for="(v,index) in defaults" :key="index" class="peach-tabs-content-item"/>
+    </div>
+  </div>
 </template>
 
 <script lang='ts' setup>
@@ -18,5 +24,31 @@ const titles = defaults?.map(tag => tag.props?.title)
 </script>
 
 <style lang="scss">
+$color: #333;
+$border-color: #d9d9d9;
+.peach-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
 
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+
+      &:first-child {
+        margin-left: 0;
+      }
+
+      &.selected {
+        color: $theme-color;
+      }
+    }
+  }
+
+  &-content {
+    padding: 8px 0;
+  }
+}
 </style>
