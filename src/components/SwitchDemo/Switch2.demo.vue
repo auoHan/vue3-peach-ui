5962033c0e7e31b7b6efac253497de8a8dff5c49
auoHan <710543268@qq.com>
2022-04-28 18:42:44 +0800

    创建Switch2.demo

---
+++ src/components/SwitchDemo/Switch2.demo.vue
@@ -0,0 +1,14 @@
+<template>
+  <PSwitch v-model:value="bool" disabled/>
+</template>
+
+<script lang='ts' setup>
+
+import PSwitch from '@/lib/Switch/PSwitch.vue'
+
+const bool = ref(false)
+</script>
+
+<style lang="scss" scoped>
+
+</style>
