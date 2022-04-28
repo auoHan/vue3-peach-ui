212099974f188d7736a7dc42f29c56e0df8a48a3
auoHan <710543268@qq.com>
2022-04-28 18:05:47 +0800

    封装Markdown

---
+++ src/components/Markdown/Markdown.vue
@@ -0,0 +1,9 @@
+// /components/Markdown.vue
+<template>
+  <article class="markdown-body" v-html="content"></article>
+</template>
+
+<script lang="ts" setup>
+// 传入的md文件
+const {content} = defineProps<{ content: string }>()
+</script>
