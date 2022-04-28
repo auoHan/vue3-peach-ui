1a438263046064f19826ec13fbebbe5df65a1f1d
auoHan <710543268@qq.com>
2022-04-28 14:02:24 +0800

    引入github-markdown-css

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,6 @@ import '@/styles/index.scss'
 import App from './App.vue'
 import router from '@/router'
 import 'virtual:svg-icons-register'
+import 'github-markdown-css'
 
 createApp(App).use(router).mount('#app')
